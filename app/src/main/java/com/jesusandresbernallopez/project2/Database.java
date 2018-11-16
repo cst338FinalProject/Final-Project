06de5a04a5d0bf41a0f036f974a528cffe97debc
Mike Menendez <mmenendez@csumb.edu>
2018-11-15 17:56:02 -0800

    Started building Account constructor. Instances will only exist after successful sql query.

--- app/src/main/java/com/jesusandresbernallopez/project2/Database.java
+++ app/src/main/java/com/jesusandresbernallopez/project2/Database.java
@@ -16,20 +16,32 @@ public class Database {
         } catch (FileNotFoundException c) {
 
             File customerdb = new File("customerTable.db");
+            String s;
             /**Customer Table Schema**
-             *CREATE TABLE customers (
+             *s = "CREATE TABLE customers (
              *name        varchar(36) not null,
              *id          numeric autoincrement,
              *email       varchar(16) not null,
              *password    varchar(16) not null,
              *username    varchar(16) not null,
              *admin       boolean,
-             *primary key (id));
+             *primary key (id));";
              */
 
             /**Project spec wants default admin generated...
-             * INSERT INTO customers (name, email, password, username, admin) admin,
-             * admin@admin.admin, !!aDMIN, !!aDMIN, true;
+             * s = "INSERT INTO customers (name, email, password, username, admin) admin,
+             * admin@admin.admin, !admiM2, !admiM2, true;";
+             */
+
+            /**Project spec wants pre-generated accounts added
+             * s = "INSERT INTO customers (name, email, password, username, admin) \"Alice\", \"alice@csumb.edu\",
+             * \"@cSit100\", \"A@lice5\", false;";
+             *
+             * s = "INSERT INTO customers (name, email, password, username, admin) \"Brian\", \"brian@csumb.edu\",
+             * \"123aBc##\", \"$BriAn7\", false;";
+             *
+             * s = "INSERT INTO customers (name, email, password, username, admin) \"Chris\", \"chris@csumb.edu\",
+             * \"CHrIS12!!\", \"!chriS12!\", false;";
              */
         }
         try {
@@ -37,20 +49,34 @@ public class Database {
             //TODO:In catch, create new file for flights table and insert default flights
             throw new FileNotFoundException();
         } catch (FileNotFoundException f) {
+            String s;
             /**Flight Table Schema**
-             * CREATE TABLE flights (
-             * id   varchar(6) not null unique,
+             * s = "CREATE TABLE flights (
+             * name         varchar(20) not null unique,
              * departLoc    varchar(20) not null,
              * destinLoc    varchar(20) not null,
-             * departTime   datetime,
+             * departTime   numeric integer,
              * flightCap    numeric not null,
              * claimedSeats numeric not null,
-             * primary key (id));
+             * price        decimal not null,
+             * primary key (id));";
              */
 
-
-            /**After table creation, project spec says need specific flights by default...
-             * INSERT INTO flights(departLoc, destinLoc, departTime, flightCap) x, y, z, a;
+            /**Project spec wants default generated flights
+             * s = "INSERT INTO flights (name, departLoc, destinLoc, departTime, flightCap, price, claimedSeats)
+             * \"Otter101\", \"Monterey\", \"Los Angeles\", 1030, 10, 150.00, 0;";
+             *
+             * s = "INSERT INTO flights (name, departLoc, destinLoc, departTime, flightCap, price, claimedSeats)
+             * \"Otter102\", \"Los Angeles\", \"Monterey\", 1300, 10, 150.00, 0;";
+             *
+             * s = "INSERT INTO flights (name, departLoc, destinLoc, departTime, flightCap, price, claimedSeats)
+             * \"Otter201\", \"Monterey\", \"Seattle\", 1100, 5, 200.50, 0;";
+             *
+             * s = "INSERT INTO flights (name, departLoc, destinLoc, departTime, flightCap, price, claimedSeats)
+             * \"Otter205\", \"Monterey\", \"Seattle\", 1545, 15, 150.00, 0;";
+             *
+             * s = "INSERT INTO flights (name, departLoc, destinLoc, departTime, flightCap, price, claimedSeats)
+             * \"Otter202\", \"Seattle\", \"Monterey\", 1410, 5, 200.00, 0;";
              */
         }
         try {
@@ -58,13 +84,14 @@ public class Database {
             //TODO:In the catch, create new file for reservations table.
             throw new FileNotFoundException();
         } catch (FileNotFoundException r) {
+            String s;
             /**Reservation Table Schema
-             * CREATE TABLE reservations(
+             * s = "CREATE TABLE reservations(
              * id   numeric not null autoincrement,
              * customerID   numeric not null,
              * seatsReq     numeric not null,
              * flightNum    numeric not null,
-             * primary key (id));
+             * primary key (id));";
              */
 
         }
