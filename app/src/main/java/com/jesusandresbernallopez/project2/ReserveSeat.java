34c3e3eba8fe8b568b666e796e443c1390e95a7f
jabernall24 <jabernall24@gmail.com>
2018-12-04 15:39:19 -0800

    Basic UI layout windows

---
+++ app/src/main/java/com/jesusandresbernallopez/project2/ReserveSeat.java
@@ -0,0 +1,23 @@
+package com.jesusandresbernallopez.project2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ReserveSeat extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_seat);
+
+    }
+
+    @Override
+    public void onClick(View v){
+
+    }
+}
