f8cf0199629ce1c08dc68318b03985a2049544ba
jabernall24 <jabernall24@gmail.com>
2018-12-14 19:26:18 -0800

    blah

--- app/src/main/java/com/jesusandresbernallopez/project2/ConfirmSeatReservation.java
+++ app/src/main/java/com/jesusandresbernallopez/project2/ConfirmSeatReservation.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.StringTokenizer;
